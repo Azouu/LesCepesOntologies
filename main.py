fbfbdca24187d1576ac13ea359f523636ec67983
Azouu <pika.azou@gmail.com>
2021-04-08 13:08:53 +0200

    change read_nasari function and match

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import mmap
 from owlready2 import *
 import os
 import numpy as np
-import gensim
-from gensim.models import KeyedVectors
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import RegexpTokenizer
+
 from collections import defaultdict
 import csv
 import sys
@@ -20,23 +15,18 @@ import pandas as pd
 
 
 def read_nasari(source):
-    nasari_vectors= {}
+    nasari_id_title = {}
+    nasari_id_vector = {}
     with open(source, encoding="utf8") as src:
         for line in csv.reader(src, delimiter='\t', quoting=csv.QUOTE_NONE):
-            nasari_vectors[line[0]] = line[1]
-    #df = pd.DataFrame(data=nasari_vectors)
-    #return df
-    return nasari_vectors
+            nasari_id_title[line[0]] = line[1]
+            nasari_id_vector[line[0]] = {}
+            dim_split = [dim.split("_") for dim in line[2:]]
+            nasari_id_vector[line[0]] = {dim: float(score) for [dim, score] in dim_split}
+    return nasari_id_title, nasari_id_vector
 
 
-def read_nasari2(source):
-    nasari_vectors= {}
-    with open(source, encoding="utf8") as src:
-        for line in csv.reader(src, delimiter='\t', quoting=csv.QUOTE_NONE):
-            nasari_vectors[line[0]] = line[2:]
-    #df = pd.DataFrame(data=nasari_vectors)
-    #return df
-    return nasari_vectors
+
 
 def flatten(liste) :
     flat_list = [item for sublist in liste for item in sublist]
@@ -47,46 +37,32 @@ def getVectInId(listeVect,vect):
         if listeVect[i] == vect:
             return i+1
 
-            
-    
 
 
-def match2(id1,id2,dico1,dico2):
-    #récuperation de la liste des vecteurs pour l'id1
-    for id,listAll in dico1.items():
-        if id == id1:
-            listID1 = [w[:12] for w in listAll]
-          
-    
-    #récuperation de la liste des vecteurs pour l'id2
-    for id,listAll in dico2.items():
-        if id == id2:
-            listID2 = [w[:12] for w in listAll]
 
-    
-    #récupération de l'intersection des vecteurs (vecteurs commun aux 2) => O dans le pdf 
-    list1set = set(listID1)
-    intersection = list(list1set.intersection(listID2))
-    
+
+def match2(id1, id2, dico1, dico2):
+    # récuperation de la liste des vecteurs pour l'id1
+    listID1 = list(dico1[id1].keys())
+
+    # récuperation de la liste des vecteurs pour l'id2
+    listID2 = list(dico2[id2].keys())
+
+
+    # récupération de l'intersection des vecteurs (vecteurs commun aux 2) => O dans le pdf
+    intersection = list(set(listID1) & set(listID2))
+
     top = 0
     bot = 0
-    
-    
-    for i in range(len(intersection)):
-        rangListe1 = getVectInId(listID1,intersection[i])
-        rangListe2 = getVectInId(listID2,intersection[i])
-        top += ((rangListe1 + rangListe2)**-1)
-        bot += (2*i+1)**-1
 
-            
+    for i in range(len(intersection)):
+        rangListe1 =  listID1.index(intersection[i]) + 1
+        rangListe2 =  listID2.index(intersection[i]) + 1
+        top += ((rangListe1 + rangListe2) ** -1)
+        bot += (2 * i + 1) ** -1
+
     return (top / bot) if bot != 0 else top
-        
-        
-        
-        
-        
-    
-    
+
 
 if __name__ == '__main__':
 
@@ -106,8 +82,12 @@ if __name__ == '__main__':
 
 
 
-    french_nasari = read_nasari2('./nasaritest2.txt')
-    print(match2("bn:00000004n","bn:00000005n",french_nasari,french_nasari))
+    french_nasari_IT, french_nasari_IV = read_nasari('./NASARI_unified_french.txt')
+    english_nasari_IT, english_nasari_IV = read_nasari('./NASARI_unified_english.txt')
+
+    print(match2("bn:00000002n","bn:00000002n",french_nasari_IV,french_nasari_IV))
+    print(french_nasari_IT["bn:00000002n"])
+    print(english_nasari_IT["bn:00000002n"])
     # english_nasari = read_nasari('./NASARI_unified_english.txt')
 
 
